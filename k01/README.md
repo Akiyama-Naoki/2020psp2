60b8de924048e48879a931d27086138ae96c3548
Naoki Akiyama <bb35319003@ms.nagasaki-u.ac.jp>
2020-04-21 14:21:54 +0900

    4/21 #1回目

--- k01/README.md
+++ k01/README.md
@@ -31,6 +31,8 @@ aa83988848 薗田光太郎
 一方，母集団の平均の推定値は，標本の平均<img src="/k01/tex/33717a96ef162d4ca3780ca7d161f7ad.svg?invert_in_darkmode&sanitize=true" align=middle width=9.39498779999999pt height=18.666631500000015pt/>が最良である．
 
 ## ソースコードの説明
-
+l.52 Nまでの平均を求める
+l.62 N-1までの平均の2乗を求める
+l.63 分散を求める
 ## 修正履歴
 
--- k01/k01.c
+++ k01/k01.c
@@ -4,7 +4,7 @@
 #include <math.h>
 
 extern double ave_online(double val,double ave)
-extern double var_online()
+extern double var_online(double val,double ave)
 
 int main(void)
 {
@@ -23,11 +23,11 @@ int main(void)
         fputs("File open error\n",stderr);
         exit(EXIT_FAILURE);
     }
-
+    
     while(fgets(buf,sizeof(buf),fp) != NULL){
         sscanf(buf,"%lf",&val);
-
-
+        ave_online(val,ave);
+        var_online(val,ave);
     
 
 
@@ -39,9 +39,30 @@ int main(void)
         exit(EXIT_FAILURE);
     }
 
-
     return 0;
 
 
 }
 
+double ave_online(double val,double ave);
+{
+    int N;
+    double f;
+    for(N = 1,N <= 14,N++)
+    {
+        f = (N - 1) / N * ave + 1 / N * val;
+    }
+    return f;
+}
+
+double var_online(double val,double ave);
+{
+     int N;
+     double ave2, g;
+     for(N = 1,N <= 14,N++)
+     {
+         ave2 = (N - 2) / (N - 1) * ave * ave + 1 / (N - 1) * val * val; 
+        g = (N - 1) / N *  1 / N * val * val - ave_online(val,ave) * ave_online(val,ave);
+     } 
+     return g;
+}
